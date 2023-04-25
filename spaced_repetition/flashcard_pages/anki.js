b7f9e9b8eee47948fd0ff7b6712a0de5ac684eec
Tanner Barbour <104728200+Tanner-Barbour@users.noreply.github.com>
2023-04-25 17:53:00 -0400

    Add files via upload

---
+++ spaced_repetition/flashcard_pages/anki.js
@@ -0,0 +1,207 @@
+key = ["Boyle's Law",
+"Henry's Law",
+"Dalton's Law",
+'Quiet breathing',
+'Forced breathing',
+'How does training affect lung volumes?',
+'Minute ventilation equation',
+'Restrictive diseases',
+'Obstructive diseases',
+'How does FEV1 differ between restrictive and obstructive?',
+'Ventilatory threshold and how it occurs',
+'How does training affect ventilatory threshold?',
+'What happens to respiration rate during hypo- and hypercapnia?',
+'How do pH, temperature and BPG affect the Hb saturation curve?',
+'How is CO2 transported?',
+'How does the body compensate for metabolic acidosis?',
+'3 things an exercise test should include for asthmatics',
+'COPD exercise prescription considerations',
+'Prolactin',
+'GH',
+'TSH',
+'ACTH',
+'FSH',
+'LH',
+'MSH',
+'ADH',
+'Oxytocin',
+'Melatonin',
+'ANP and BNP',
+'Calcitriol',
+'EPO',
+'Renin',
+'Response of catecholamines, insulin/glucagon, and thyroid hormones to training',
+'Diabetic ketoacidosis',
+'Blood glucose levels for diabetes',
+'HbA1C levels for diabetes',
+'Why is exercise important for diabetics?',
+'Frequency that diabetics should get exercise',
+'Types of phagocytes',
+'How do NK cells kill?',
+'IFN-alpha',
+'IFN-gamma',
+'iFN-beta',
+'Results of the complement system',
+'Antibody-mediated immune response',
+'Cell-mediated immune response',
+'Cytotoxic T cells',
+'Helper T cells',
+'Memory T cells',
+'Tregs (regulatory T cells)',
+'How do cytotoxic T cells kill?',
+'How do Abs work?',
+'IgG',
+'IgE',
+'IgD',
+'IgA',
+'IgM',
+'How do HIV/AIDS affect the immune system?',
+'How does ART therapy affect the body and what type of exercise can work to overcome this?',
+'HIV exercise prescription considerations',
+'Naive B-cells',
+'Memory cells',
+'Plasma cells',
+'Thyroid hormone',
+'Calcitonin',
+'PTH',
+'Insulin',
+'Glucagon',
+'Inspiration muscles',
+'Expiration muscles',
+'Accessory muscles',
+'Aldosterone',
+'Cortisol',
+'Adrenal androgens',
+'Catecholamines',
+'Calcitriol increases the transcription of'];
+
+item = ['if you decrease the volume of a container of gas and hold the temperature constant, the pressure from the gas will increase',
+'At a given temperature the solubility of a gas in a solution is directly proportional to the pressure of the gas',
+'each gas contributes to total pressure in proportion to its relative abundance',
+'no muscles are contracted, inhalation muscles (diaphragm and external intercostals) relax',
+'Active process; internal intercostals depress the ribs and abdominal muscles compress the abdomen',
+'Decreases functional residual capacity and residual volume, increases vital capacity',
+'VE = f  TV (breaths/min  tidal volume)',
+'pulmonary fibrosis, obesity, scoliosis, muscular dystrophy',
+'COPD, asthma, cystic fibrosis',
+'FEV1 is normal in restrictive and FVC is reduced. FEV1 is reduced compared to FVC in obstructive.',
+'The point at which the volume of air inhaled per minute increases at an exponential rate compared to the increase in workload. Can be due to a drop in pH which increases CO2 from the bicarbonate system',
+'can delay onset of ventilatory threshold',
+'Hypercapnia doubles the RR, hypocapnia decreases in PCO2 and RR decreases',
+'Drop in pH, increase in temp, and & BPG binds to Hb which releases more O2',
+'70% as carbonic acid, 20% bound to Hb, 10% dissolved in plasma',
+'Increased respiratory rate to eliminate CO2 in the lungs\nIncreased secretion of H+ \nReabsorption of HCO3 in the kidney',
+'Cardiopulmonary capacity/CRF\nPulmonary function\nOxyhemoglobin saturation (pulse ox)',
+'- Higher intensities yield greater physiologic results\n- Blood oxygen should be measured at least the first exercise training session\n- Resistance training should be a mandatory part to address muscle dysfunctions (upper body to reduce dyspnea and lower body to reduce risk of falls)',
+'stimulates mammary gland development and stimulates milk production',
+'stimulates cell growth and replication through protein synthesis indirectly',
+'release thyroid hormones from thyroid gland, iodide uptake, production of thyroglobulin and thyroid peroxidase',
+'release of glucocorticoids and adrenal androgens from adrenal cortex',
+'gonadotropin; promotes follicle development in females and sperm maturation in males',
+'leads to ovulation in females and testosterone production in males',
+'stimulates melanocytes in the skin to increase melanin production',
+'Acts on kidneys to retain water and decrease urination, can cause vasoconstriction which elevates BP',
+'Stimulates smooth muscle contraction in uterus and milk ejection, linked to sexual activity by stimulating sperm ejaculation in men and promoting sperm to travel into fallopian tubes in women',
+'produced by pineal gland to regulate circadian cycle; higher at night',
+'leads to a decrease in blood volume and BP',
+'Ca2+ and PO4 absorption in the gut\nActs with PTH in Ca2+ homeostasis',
+'Stimulates RBC production which increases blood volume and improves O2 delivery',
+'- Released by juxtaglomerular cells in kidney and converts angiotensinogen to angiotensin I\n- ACE converts angiotensin I to angiotensin II to stimulate adrenal gland to release aldosterone, ADH \n- Increases HR and BP',
+'- Higher resting concentrations of catecholamines\n- Increases circulating levels of T3 and T4\n- Decreases insulin production and increases in receptor sensitivity\n- Increases glucagon resulting in more glycogenolysis and gluconeogenesis',
+'Cells cannot take in glucose, therefore FA are primary energy source. Excess lipolysis/beta oxidation produces excess acetyl CoA which combines to form ketones (for the brain)',
+'>= 126 mg/dl',
+'>= 6.5%',
+'It increases sensitivity to insulin',
+'3-7 days/week of aerobic training, minimum of 2x/week of resistance training; should not go more than 2 consecutive days without PA',
+'Attach to pathogens via adhesion; microphages (neutrophils and eosinophils) and macrophages (fixed and free)',
+'Activation steps:\n1. recognition and adhesion\n2. realignment of the golgi \n3. secretion of perforins by exocytosis \n4. lysis of abnormal cell by creating pores in the target',
+'Protects other cells and attracts NK cells',
+'Stimulates macrophages to produce NO, which works as an antiviral molecule',
+'Slows inflammation',
+'- Membrane attack complex (MAC) --> which produces pores in the pathogen\n- Opsonization --> macrophages can bind to complement proteins and destroy pathogen\n- Histamine release by mast cells & basophils --> increased inflammation and blood flow to the area of injury',
+'- B-cells\n- Responds to antigens traveling freely \n- Antibodies are programmed to recognize and bind specific antigens so they are easily recognized by phagocytes',
+'- T-cell\n- Responds to cellular pathogens or abnormal cells \n- Cytotoxic T-cells cause cells to burst',
+'directly attacks pathogens physically and chemically',
+'stimulates both T- and B-cell response (CD4+)',
+'remember the specific pathogen',
+'moderates the immune response by inhibiting T and B cells',
+'Release perforin, release lymphotoxin, or initiates apoptosis in target cell',
+'1. Neutralization\n2. Precipitation and agglutination\n3. Activation of the complement system\n4. Attraction of phagocytes\n5. Opsonization\n6. Stimulation of inflammation\n7. Prevention of pathogen adhesion',
+'largest amount of antibody released',
+'allergies',
+'stuck in cell surface',
+'in granular secretions',
+'first secreted then IgG secreted',
+'Kills helper T cells\n- production can match destruction\n- destruction outpaces production\n- severe depletion of helper T cells',
+'Increases lifespan, higher risk of osteopenia; weight-bearing exercise',
+'- Contact sports & high-risk sports should be avoided \n- Exercise should be supervised in those who are symptomatic\n- If patient complains of dizziness, swollen joints, or is vomiting, exercise should be precluded',
+'contain antibodies (IgD) on their surface with specific receptors for an antigen; differentiates into memory and plasma cells',
+'remembers the antigen and protects the host from a second infection with the same bacteria',
+'secretes antibodies',
+'- Increases BMR and ATP production \n- T3 is more active than T4 and can be converted by enzymes in the kidney, liver and other tissues',
+'Decreases blood Ca2+ levels, inhibits osteoclast activity, stimulates Ca2+ loss by the kidneys',
+'stimulates osteoblasts to make RANKL, enhances Ca2+ absorption in the kidney when blood Ca2+ is low, activates 1-hydroxylase which converts calcidiol to calcitriol and acts on target tissues and increases transcription of genes',
+'anabolic, increase in glucose uptake via GLUT4, dephosphorylates enzymes, parasympathetic activation increases insulin secretion',
+'catabolic; activates adenylate cyclase via G protein; increases with strenuous exercise',
+'sternocleidomastoid, scalenes, external intercostals, diaphragm, parasternal intercostals',
+'internal intercostals, external abdominal oblique, internal abdominal oblique, transversus abdominis, recuts abdominis',
+'Sternocleidomastoid, scalenes',
+'- Stimulates retention of Na+ and elimination of K+\n- Due to drop in Na+ in the blood, decrease in blood volume, or increase in blood K+',
+'Increased rates of gluconeogenesis and lipolysis; anti-inflammatory effect',
+'Small quantities in response to ACTH; can be converted to adrenal estrogen stimulating pubic hair growth',
+'Binds to alpha1 and beta1 adrenergic receptors',
+'TRPV6, PMCA, and Calbindin'];
+
+function randomNumber(min, max) {
+    return Math.random() * (max - min) + min
+}
+
+function addCard(){
+    var newKey = prompt("Give me your key:");
+    var newValue = prompt("Give me your value: ");
+    console.log(newKey)
+    console.log(newValue)
+    key.push(newKey)
+    item.push(newValue)
+    let table = document.getElementById("myTable");
+    let row =  table.insertRow(-1);
+    let c1 = row.insertCell(0)
+    let c2 = row.insertCell(1)
+    c1.innerText = key.slice(-1)
+    c2.innerText = item.slice(-1)
+}
+//const sleep = ms => new Promise(res => setTimeout(res, ms));
+var i = 0;
+var value = 0;
+
+function submitMe() {
+    document.getElementById("headertag").innerHTML = key[value];
+    i ++;
+    console.log(i)
+    revealMe();
+}
+
+function revealMe(){
+    if (i%2===0) {
+        document.getElementById("headertag").innerHTML = item[value];
+      } else {
+        value = Math.round(randomNumber(0,item.length));
+        document.getElementById("headertag").innerHTML = key[value];
+      }
+      
+}
+//async function revealMe(){    
+//    await sleep(10000);
+//    document.getElementById("answertag").innerHTML = item[value];
+//}
+
+function completeTable(){
+    for(var i=0;i<key.length;i++){
+        let table = document.getElementById("myTable");
+        let row =  table.insertRow(-1);
+        let c1 = row.insertCell(0)
+        let c2 = row.insertCell(1)
+        c1.innerText = key[i]
+        c2.innerText = item[i]
+    }
+}
